688770d89e6d64713a392e05ef89a76e9c47f842
Konstantin8105 <konstantin8105@gmail.com>
2019-06-01 15:09:03 +0300

    success of running

--- md.go
+++ md.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/russross/blackfriday"
@@ -49,11 +50,6 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	if len(os.Args) < 2 || flag.NArg() < 1 {
-		flag.PrintDefaults()
-		os.Exit(0)
-	}
-
 	// flag action
 	if *help {
 		flag.PrintDefaults()
@@ -61,7 +57,7 @@ func main() {
 	}
 
 	// output used server port
-	fmt.Fprintf(os.Stdout, "Start server on port :%s", *port)
+	fmt.Fprintf(os.Stdout, "Start server on port :%s\n", *port)
 
 	// prepare server
 
@@ -80,17 +76,53 @@ func main() {
 
 // mainHandler generate main web page with list of articles
 func mainHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(os.Stdout, "GET : %v", r.URL.Path)
+	fmt.Fprintf(os.Stdout, "GET : %v\n", r.URL.Path)
 
-	// find all markdown files
-	// get names for each file
-	// generate markdown main page
-	// generate html by markdown
+	if err := func() (err error) {
+		defer func() {
+			if err != nil {
+				err = fmt.Errorf("Try open page: %v. %v", r.URL.Path, err)
+			}
+		}()
+		// find all markdown files
+		var mdFiles []string
+		if err = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", path, err)
+				return err
+			}
+			if info.IsDir() {
+				return nil
+			}
+			// get filename markdown files
+			if strings.HasSuffix(info.Name(), ".md") {
+				mdFiles = append(mdFiles, info.Name())
+			}
+			return nil
+		}); err != nil {
+			return fmt.Errorf("Cannot walk: %v", err)
+		}
+		// generate markdown main page
+		var mainTmpl string = "# Основная страница со списком статей.\n"
+		for _, file := range mdFiles {
+			// get name of article
+			var name string = file
+
+			// add to main page
+			mainTmpl += fmt.Sprintf("\n* [%s](/articles/%s)\n", name, file)
+		}
+		// generate html by markdown
+		html := blackfriday.MarkdownCommon([]byte(mainTmpl))
+		fmt.Fprintf(w, tmpl, html)
+		return
+	}(); err != nil {
+		fmt.Fprintf(w, "Error : %v", err)
+	}
 }
 
 // articleHandler generate web page with article
 func articleHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(os.Stdout, "GET : %v", r.URL.Path)
+	fmt.Fprintf(os.Stdout, "GET : %v\n", r.URL.Path)
 
 	if err := func() (err error) {
 		defer func() {
@@ -107,7 +139,7 @@ func articleHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		// get file content
 		var content []byte
-		content, err = ioutil.ReadFile(title + ".md")
+		content, err = ioutil.ReadFile(title)
 		if err != nil {
 			err = fmt.Errorf("Cannot read file: %v", err)
 			return
